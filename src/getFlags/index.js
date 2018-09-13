b583a24e96686a8e9a117f9857ed7a3a0271dabc
Oleg Zinchenko <olegzinchenko1983@gmail.com>
2018-09-13 20:47:53 +0300

    add getFlags

--- src/getFlags/index.js
+++ src/getFlags/index.js
@@ -7,6 +7,16 @@
  */
 const getFlags = arr => {
   /* your logic here...*/
+  let newArr = [];
+  for(let i = 0; i< arr.length; i++) {
+    for(let j = 0; j < arr[i].flags.length; j++) {
+      newArr.push(arr[i].flags[j]);
+    }
+  }
+  return newArr;
 };
+
+const arr = [{flags: [1, 2, 3]}, {flags: [4, 5, 6]}];
+getFlags(arr); // [1, 2, 3, 4, 5, 6]
 
 export default getFlags;
